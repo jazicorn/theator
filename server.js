c6192f2dde57cde44762beda3493c60417f21e10
Jasmine Anderson <jazz2900@gmail.com>
2018-09-11 14:20:26 -0700

    template

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const router = express.Router();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +22,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+router.get('/theator/name-of-route', (req, res) => {
+    res.json({'stub': '[${req.originalUrl}] Endpoint works! Replace me in Step 2.'});
+});
+
+module.exports = router;
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,3 +46,5 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+"hi"
